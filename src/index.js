76e50578f2b510083c54b2830dadb8f0e462de3d
Kuzya <alexkuz@gmail.com>
2015-06-20 13:08:45 +0300

    treat if-else statements as (..., ...)

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ const processHTML = {
     const config = new CLIEngine({ useEslintrc: true }).getConfigForFile(path.resolve(filename));
     const quote = config.rules.quotes[1] === 'double' ? '"' : '\'';
     text = text;
-    return [text
+    const processed = text
     // replace jinja comments with js comments
-      .replace(/\{#.*?#\}/g, (str, val) => `/*${val}*/`)
+      .replace(/\{#(.*?)#\}/g, (str, val) => `/*${val}*/`)
+    // treat if-else statement as ( ... , ... )
+      .replace(/\{%(-?\s*if.*?)%\}/g, (str, val) => `(/*${val.substr(1)}*/`)
+      .replace(/\{%(-?\s*(else|elif).*?)%\}/g, (str, val) => `,/*${val.substr(1)}*/`)
+      .replace(/\{%(-?\s*endif\s*-?)%\}/g, (str, val) => `/*${val.substr(1)}*/)`)
     // replace jinja statements with js comments
-      .replace(/\{%.*?%\}/g, (str, val) => `/*${val}*/`)
+      .replace(/\{%(.*?)%\}/g, (str, val) => `/*${val}*/`)
     // replace jinja expression tags in strings with spaces
       .replace(/(['"])(.*?)\1/g, str => str.replace(/(\{\{|\}\})/g, '  '))
     // replace jinja expressions with strings
-      .replace(/\{\{(.*?)\}\}/g, (str, val) => `${quote} ${val} ${quote}`)]; 
+      .replace(/\{\{(.*?)\}\}/g, (str, val) => `${quote} ${val} ${quote}`);
+    return [processed];
   },
 
   postprocess(messages) {
