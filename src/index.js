9f1790d884bdd1ea4f03c23db000a0106b9c3925
Elad Avron <elad.avron@intel.com>
2022-01-09 11:22:24 +0200

    Upgraded plugin to work with modern (8.0+) ESLint

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
 'use strict';
-import { CLIEngine } from 'eslint';
-import path from 'path';
 
 const processHTML = {
-  preprocess(text, filename) {
-    const config = new CLIEngine({ useEslintrc: true }).getConfigForFile(path.resolve(filename));
-    const quote = config.rules.quotes[1] === 'double' ? '"' : '\'';
-    text = text;
-    const processed = text
-    // replace jinja comments with js comments
-      .replace(/\{#([\s\S]*?)#\}/g, (str, val) => `/*${val}*/`)
-    // treat if-else statement as ( ... , ... )
-      .replace(/\{%(-?\s*if.*?)%\}/g, (str, val) => `(/*${val.substr(1)}*/`)
-      .replace(/\{%(-?\s*(else|elif).*?)%\}/g, (str, val) => `,/*${val.substr(1)}*/`)
-      .replace(/\{%(-?\s*endif\s*-?)%\}/g, (str, val) => `/*${val.substr(1)}*/)`)
-    // replace jinja statements with js comments
-      .replace(/\{%(.*?)%\}/g, (str, val) => `/*${val}*/`)
-    // replace jinja expression tags in strings with spaces
-      .replace(/\{[{%]([\s\S]*?)[}%]\}/g, str => str.replace(/['"]/g, ' '))
-      .replace(/(['"])(.*?)\1/g, str => str.replace(/(\{\{|\}\})/g, '  '))
-    // replace jinja expressions with strings
-      .replace(/\{\{(.*?)\}\}/g, (str, val) => `${quote} ${val} ${quote}`);
-    return [processed];
-  },
+    preprocess(text, _) {
+        text = text;
+        var processed = text
+            // replace jinja comments with js comments
+            .replace(/\{#([\s\S]*?)#\}/g, (str, val) => `/*${val}*/`)
 
-  postprocess(messages) {
-    return messages[0];
-  }
+            // treat if-else statement as ( ... , ... )
+            .replace(/\{%(-?\s*if.*?)%\}/g, (str, val) => `/*${val.substr(1)}*/`)
+            .replace(/\{%(-?\s*(else|elif).*?)%\}/g, (str, val) => `/*${val.substr(1)}*/`)
+            .replace(/\{%(-?\s*endif\s*-?)%\}/g, (str, val) => `/*${val.substr(1)}*/`)
+
+            // replace jinja statements with js comments
+            .replace(/\{%(.*?)%\}/g, (str, val) => `/*${val}*/`)
+
+            // replace jinja expression tags in strings with spaces
+            .replace(/\{[{%]([\s\S]*?)[}%]\}/g, str => str.replace(/['"]/g, ' '))
+            .replace(/(['"])(.*?)\1/g, str => str.replace(/(\{\{|\}\})/g, '  '))
+
+            // replace jinja expressions with a regex pattern - which is independent of quotes.
+            // It is padded to the length of the expression to avoid misaligning anything after it inline.
+            .replace(/\{\{(.*?)\}\}/g, str => "/" + ("/".padStart(str.length, 0)));
+        return [processed];
+    },
+
+    postprocess(messages) {
+        return messages[0];
+    }
 };
 
-export default {
-  processors: {
-    '.js': processHTML,
-    '.html': processHTML,
-    '.htm': processHTML
-  }
+module.exports = {
+    processors: {
+        '.js': processHTML,
+        '.html': processHTML,
+        '.htm': processHTML
+    }
 };
